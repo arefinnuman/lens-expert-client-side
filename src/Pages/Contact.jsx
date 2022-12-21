c8f8bad0121ea61c160f9b884c5afd89ea046da6
arefinnuman <arefinnuman2000@gmail.com>
2022-12-21 17:40:20 +0600

    Project Polished

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -51,10 +51,7 @@ const Contact = () => {
               </p>
             </div>
           </div>
-          <form
-            novalidate=""
-            className="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid"
-          >
+          <form className="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid">
             <label className="block">
               <span className="mb-1">Full name</span>
               <input
